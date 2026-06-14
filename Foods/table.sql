41d3c654aec7f95985ebc2d4225be6b14b492ca3
rijeshh <sampangrijesh071@gmail.com>
2026-06-14 10:17:41 +0545

    table

---
+++ Foods/table.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS foods;
+CREATE TABLE foods (
+		item_name VARCHAR,
+		item_category VARCHAR,
+		item_subcategory VARCHAR,
+		brand_name VARCHAR,
+		brand_name VARCHAR,
+		brand_tier VARCHAR,
+		country VARCHAR,
+		city VARCHAR,
+		city_tier VARCHAR,
+		price_usd_normalized FLOAT,
+		serving_size_g FLOAT,
+		calories FLOAT,
+		protein_g FLOAT,
+		total_fat_g FLOAT,
+		total_carbs_g FLOAT,
+		sodium_mg FLOAT,
+		sugars_g FLOAT,
+		avg_rating FLOAT,
+		is_bestseller BOOL
+);
+SELECT
+* FROM
+foods limit 10;
