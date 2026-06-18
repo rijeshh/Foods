043512036b83f92c7ac2f33d2dc5f2f1114b9896
rijeshh <sampangrijesh071@gmail.com>
2026-06-18 13:44:57 +0545

    18

--- Foods/sql_analysis.sql
+++ Foods/sql_analysis.sql
@@ -13,20 +13,6 @@ ORDER BY
     avg_calories DESC;
 
 
------- brand performance (avg ratings and price) -----------------------
-
-SELECT
-    item_name,
-    ROUND(AVG(avg_rating) :: numeric, 2) AS rating,
-    ROUND(AVG(price_usd_normalized) :: numeric, 2) AS avg_price
-FROM 
-    foods
-GROUP BY 
-    item_name
-ORDER BY 
-    rating DESC
-LIMIT 10;
-
 ------------ country price and ratings (avg ratings and price) -----------------------
 
 SELECT
@@ -55,4 +41,59 @@ GROUP BY
     item_subcategory
 ORDER BY
     protein_efficiency_per_100_calories DESC
+LIMIT 10;
+
+
+---------- sodium warning items --------------------
+
+SELECT
+    item_category,
+    ROUND(AVG(sodium_mg) :: numeric, 2) as avg_sodium_mg
+FROM
+    foods
+WHERE
+    sodium_mg > 800
+GROUP BY
+    item_category
+ORDER BY
+    avg_sodium_mg DESC
+
+
+------ brand performance (avg ratings and price) -----------------------
+
+SELECT
+    item_category,
+    ROUND(AVG(avg_rating) :: numeric, 2) AS rating,
+    ROUND(AVG(price_usd_normalized) :: numeric, 2) AS avg_price
+FROM 
+    foods
+GROUP BY 
+    item_category
+ORDER BY 
+    rating DESC
+LIMIT 100;
+
+----------- city tier pricing and ratings -----------------------
+
+SELECT
+    city_tier,
+    ROUND(AVG(price_usd_normalized) :: numeric, 2) AS avg_price,
+    ROUND(AVG(avg_rating) :: numeric, 2) AS avg_rating
+FROM 
+    foods 
+GROUP BY 
+    city_tier
+ORDER BY 
+    avg_price DESC;
+
+----------- top sugar content items --------------------
+
+SELECT
+    item_name,  
+    item_category,
+    sugars_g
+FROM
+    foods
+ORDER BY
+    sugars_g DESC
 LIMIT 10;
