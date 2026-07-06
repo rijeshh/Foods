96d72bb0e3b9fff700db23eef85d75f9afd1d196
rijeshh <sampangrijesh071@gmail.com>
2026-07-06 17:25:36 +0545

    22

--- Foods/sql_analysis.sql
+++ Foods/sql_analysis.sql
@@ -153,17 +153,11 @@ SELECT
 FROM fast_food
 ORDER BY value_score DESC
 LIMIT 10;
-SELECT
-    item_name,
-    item_category,
-    brand_name,
-    country,
-    price_usd_normalized,
-    avg_rating,
-    ROUND(avg_rating / NULLIF(price_usd_normalized, 0), 3) AS value_score
-FROM fast_food
-ORDER BY value_score DESC
-LIMIT 10;
+
+
+------- value for money analysis (rating to price ratio) -----------------------
+
+
 SELECT
     item_name,
     item_category,
